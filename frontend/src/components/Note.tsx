e71ea5e69a2e9d9c77a9c1392635d6ed3f8b2f91
JonathanZhang <jonathanzhang@ucsb.edu>
2024-08-13 15:44:24 -0700

    Done!

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,4 +1,4 @@
-import react from 'react'
+// import react from 'react'
 import "../styles/Note.css"
 interface Note {
     id: number;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import api from "../api";
 import Note from "../components/Note"
 import "../styles/Home.css"
